499b30ee377db412f107f6074d750ac9f6cc8f8f
春天快来 <81024480@qq.com>
2017-02-23 10:22:13 +0800

    添加文件

---
+++ 添加到购物车动画/添加到购物车动画/sssTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  sssTableViewCell.h
+//  添加到购物车动画
+//
+//  Created by Youthmedia-A20 on 2017/2/23.
+//  Copyright © 2017年 cuckoopay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sssTableViewCell : UITableViewCell
+
+@end
---
+++ 添加到购物车动画/添加到购物车动画/sssTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  sssTableViewCell.m
+//  添加到购物车动画
+//
+//  Created by Youthmedia-A20 on 2017/2/23.
+//  Copyright © 2017年 cuckoopay. All rights reserved.
+//
+
+#import "sssTableViewCell.h"
+
+@implementation sssTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
