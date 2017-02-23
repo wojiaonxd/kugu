ed0bd584ab8dfa5c683a6e33766f746e9e812ea3
春天快来 <81024480@qq.com>
2017-02-23 10:17:41 +0800

    动画

---
+++ 添加到购物车动画/添加到购物车动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  添加到购物车动画
+//
+//  Created by Youthmedia-A20 on 16/10/17.
+//  Copyright © 2016年 cuckoopay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 添加到购物车动画/添加到购物车动画/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  添加到购物车动画
+//
+//  Created by Youthmedia-A20 on 16/10/17.
+//  Copyright © 2016年 cuckoopay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
